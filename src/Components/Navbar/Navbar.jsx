234687bbba1c9ee51865c336aa636764437413fd
Jemma <jjsplodge@hotmail.com>
2021-06-20 22:25:13 +0100

    Nav bar create but bug needs fixing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
-import  { VscChevronDown } from 'react-icons/vsc';
+import  { VscChevronDown, VscChromeClose } from 'react-icons/vsc';
 
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [ sidebar, setSidebar ] = useState(false)
+
+  const showSidebar = () => {
+    setSidebar(!sidebar)
+    console.log(showSidebar);
+  }
   return (
-    <div className={styles.nav}>
-      
-      <p><VscChevronDown /></p>
-      {/* <h3><Link to='/'>Home</Link></h3>
-      <h3><Link to='/about'>About</Link></h3>
-      <h3><Link to='/projects'>Projects</Link></h3>
-      <h3><Link to='/contact'>Contact</Link></h3> */}
-     
-    </div>
+    <>
+      <div className={styles.nav} onClick={showSidebar}>
+        <VscChevronDown />
+      </div>
+      <nav className={sidebar ? 'navmenu active' : 'navmenu'}>
+        <ul className={styles.navmenuitems}>
+          <li className={styles.navmenutoggle}><Link to='/'>Home</Link></li>
+          <li className={styles.navmenutoggle}><Link to='/about'>About</Link></li>
+          <li className={styles.navmenutoggle}><Link to='/projects'>Projects</Link></li>
+          <li className={styles.navmenutoggle}><Link to='/contact'>Contact</Link></li>
+          <VscChromeClose />
+        </ul>
+      </nav>
+    </>
   );
 };
 
