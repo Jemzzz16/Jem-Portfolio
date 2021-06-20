7461003a338c8b61c739251ce4298651928d2417
Jemma <jjsplodge@hotmail.com>
2021-06-20 14:50:32 +0100

    about page and projects added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,10 +7,30 @@ import Profiledimond from '../../images/Profile-pic.png';
 const About = () => {
   return (
     <div className={styles.aboutmain}>
-      <p>About works</p>
       <img className={styles.pinkdimond} src={Pinkdimond} alt='#'></img>
       <img className={styles.profiledimond} src={Profiledimond} alt='#'></img>
       <img className={styles.greendimond} src={Greendimond} alt='#'></img>
+      <div className={styles.paraone}>
+        <p>
+          Hi I’m Jem, full stack 
+          Web developer, photographer 
+          and enthusiastic proffesional.
+        </p>
+      </div>
+      <div className={styles.paratwo}>
+        <p>
+          Let me create, 
+          capture & develop 
+          your ideas 
+          into something amazing!
+        </p>
+      </div>
+      
+      <div className={styles.projectheadings}>
+        <h3>.Project 1 /</h3>
+        <h3>.Project 2 /</h3>
+        <h3>.Project 3 /</h3>
+      </div>
     </div>
   );
 };
